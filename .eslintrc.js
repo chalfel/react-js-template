870bc754ff67faf51cf80cb5885a3d18fb51044b
chalfel <caiohalcsik@gmail.com>
2021-03-05 08:15:28 -0300

    feat: wip

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "standard",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "prettier/prettier": ["error", {}, { usePrettierrc: true }], // Use our .prettierrc file as source
+  },
+};
